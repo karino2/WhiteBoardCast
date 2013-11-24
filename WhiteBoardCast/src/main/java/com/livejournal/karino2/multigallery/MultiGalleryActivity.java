38802ff2395b3b501a37bc2fcb120cd3d19ea4f8
karino <hogeika2@gmail.com>
2013-11-24 20:48:54 +0900

    Add basic menu and icons. Need to create new icons.

--- WhiteBoardCast/src/main/java/com/livejournal/karino2/multigallery/MultiGalleryActivity.java
+++ WhiteBoardCast/src/main/java/com/livejournal/karino2/multigallery/MultiGalleryActivity.java
@@ -121,7 +121,9 @@ public class MultiGalleryActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_done) {
+            setResultToResultPaths();
+            finish();
             return true;
         }
         if(id == android.R.id.home && isAlbum) {
